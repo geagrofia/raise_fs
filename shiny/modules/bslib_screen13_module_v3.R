96fb79d59aec9e5a4a89d4473687a6b0a7be1751
geagrofia <andrewfarrow72@gmail.com>
2025-09-25 10:59:13 +0200

    Scrollable DT table added. Interface panel design changed. Name changes to shared parameter reactive values. Help button added. Progress saved.

--- shiny/modules/bslib_screen13_module_v3.R
+++ shiny/modules/bslib_screen13_module_v3.R
@@ -1,46 +1,113 @@
-bslib_screen13_module_v3_SidebarUI <- function(id, shared_values) {
+bslib_screen13_module_v3_SidebarUI <- function(id, shared_values, shared_parameters) {
   
   ns <- NS(id)
   
   tagList(
-    h3("Innovation 1 Summary:"),
-    checkboxInput(ns("triangulation"), "Does this innovation have triangulation data from local experts?", value = FALSE),
-    
-       # UI actionButtons screen navigation ----
-    actionButton(ns("back_to_screen12"), "Back to Screen 12"), 
-    
-    # selectInput(ns("faoclass1"), "Number of levels to map FAO class:", choices = c(1:3), selected = 3), 
-    # selectInput(ns("limitsclass1"), "Number of levels to map limitations:", choices = c(1:3), selected = 3), 
-    # selectInput(ns("concclass1"), "Number of levels to map limitations:", choices = c(1:3), selected = 3), 
-    uiOutput(ns("dynamic_run_inn2"))
+    # wellPanel(
+    #   style = "padding: 10px; margin-bottom: 5px;",
+    #   actionButton(
+    #     ns("back_to_screen12"),
+    #     label = tagList(
+    #       icon("circle-left"),
+    #       # icon first
+    #       "Back to Screen 12"
+    #       # text second
+    #     ),
+    #     class = "btn-primary"
+    #   ),
+    #   actionButton(
+    #     ns("add_inn"),
+    #     label = tagList(
+    #       "Add innovation",
+    #       # text first
+    #       icon("circle-right")  # icon second)
+    #     ),
+    #     class = "btn-primary"
+    #   )
+    # ),
+    wellPanel(
+      style = "padding: 10px; margin-bottom: 5px;",
+      div(
+        style = "display:inline-block;vertical-align:middle;margin-bottom: 5px;",
+        actionButton(
+          ns("show_help_13_01"),
+          title = "Help for Step 13",
+          label = tagList(
+            icon("circle-question")  # icon second)
+          ),
+          style = "background: rgba(23, 162, 184, 0.5);"
+        )
+        
+      ),
+      div(
+        style = "display: inline-block; vertical-align: middle; margin-left: 10px;",
+      h4("Step 13: Run IRM or start Innovation 2")
+      ),
+      checkboxInput(ns("triangulation"), "Does this innovation have triangulation data from local experts?", value = FALSE),
+      #,
+      # selectInput(ns("faoclass1"), "Number of levels to map FAO class:", choices = c(1:3), selected = 3), 
+      # selectInput(ns("limitsclass1"), "Number of levels to map limitations:", choices = c(1:3), selected = 3), 
+      # selectInput(ns("concclass1"), "Number of levels to map limitations:", choices = c(1:3), selected = 3), 
+      uiOutput(ns("dynamic_run_inn2"))
+    ),
+    wellPanel(
+      style = "padding: 10px; margin-bottom: 5px;",
+      h4("Review Innovation 1"),
+      scrollable_DT(ns("summary_table"))
     )
+  )
 }
 
 bslib_screen13_module_v3_MainUI <- function(id) {
   ns <- NS(id)
-    tagList(
-    textOutput(ns("value_display")),
-    textOutput(ns("level_display")),
-    textOutput(ns("selection_display")),
-    textOutput(ns("spatres_display")),
-    textOutput(ns("aggregation_display")),
-    textOutput(ns("num_innovations_display")),
-    textOutput(ns("innovation_system_display")),
-    textOutput(ns("crop_1_display")),
-    textOutput(ns("ideotype_1_display")),
-    textOutput(ns("scenario_1_display")),
-    textOutput(ns("inn_type_1_display")),
-    DTOutput(ns("summary_table"))
+  tagList(
+    wellPanel(
+      h4("Navigate", style = "color: var(--bs-secondary);"),
+      style = "padding: 10px; margin-bottom: 5px;",
+      actionButton(
+        ns("back_to_screen12"),
+        title = "Go back to Step 12: View or Edit Spatial Data",
+        label = tagList(
+          icon("circle-left"),
+          # icon first
+          #"Go to Introduction"
+          "Back"
+          # text second
+        ),
+        class = "btn-primary"
+      ),
+      
+      tags$span(
+        tagList("Step 13", icon("location-crosshairs")),
+        # text + icon
+        class = "btn btn-info disabled"
+      ),
+      uiOutput(ns("dynamic_navbutn"))
+    ),
+    wellPanel(
+      style = "padding: 10px; margin-bottom: 5px; background: rgba(23, 162, 184, 0.5);",
+      h4("Summary of IRM setup"),
+      textOutput(ns("value_display")),
+      textOutput(ns("level_display")),
+      textOutput(ns("selection_display")),
+      textOutput(ns("spatres_display")),
+      textOutput(ns("aggregation_display")),
+      textOutput(ns("num_innovations_display")),
+      textOutput(ns("innovation_system_display")),
+      textOutput(ns("crop_1_display")),
+      textOutput(ns("ideotype_1_display")),
+      textOutput(ns("scenario_1_display")),
+      textOutput(ns("inn_type_1_display")),
+      textOutput(ns("tri_1_display"))
+    )
   )
 }
 
-bslib_screen13_module_v3_Server <- function(id, shared_values, switch_screen) {
+bslib_screen13_module_v3_Server <- function(id, shared_values, shared_parameters, switch_screen) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     
     NA_weights <- 0
-    
-    message("S13. session$ns")
     
     # Load the initial data ----
     initial_summary_data <- reactive({
@@ -49,22 +116,22 @@ bslib_screen13_module_v3_Server <- function(id, shared_values, switch_screen) {
       
       if (file.exists(paste0(
         "E:/repos/raise_fs/shiny/data/",
-        shared_values$crop_name_1,
+        shared_parameters$crop_name_1,
         "_",
-        shared_values$ideotype_1,
+        shared_parameters$ideotype_1,
         "_",
-        shared_values$scenario_1,
+        shared_parameters$scenario_1,
         "_requirements_s12.csv"
       ))) {
         
         df_requirements_s12 <- read.csv(
           paste0(
             "E:/repos/raise_fs/shiny/data/",
-            shared_values$crop_name_1,
+            shared_parameters$crop_name_1,
             "_",
-            shared_values$ideotype_1,
+            shared_parameters$ideotype_1,
             "_",
-            shared_values$scenario_1,
+            shared_parameters$scenario_1,
             "_requirements_s12.csv"
           )
         )
@@ -113,7 +180,8 @@ bslib_screen13_module_v3_Server <- function(id, shared_values, switch_screen) {
           selection = list(mode = "none"),
           extensions = c('FixedColumns', 'FixedHeader'),
           editable = FALSE,
-          options = list(lengthMenu = c(10, 20, 50), 
+          options = list(scrollX = TRUE,
+            lengthMenu = c(10, 20, 50), 
                          pageLength = 10, 
                          scrollX = TRUE,
                          fixedHeader= TRUE,
@@ -125,92 +193,95 @@ bslib_screen13_module_v3_Server <- function(id, shared_values, switch_screen) {
     
     # observe parameters ----
     observe({
-      message("S13. observe parameters")
-      shared_values$triangulation <- ifelse(input$triangulation, 1, 0)
-      message("S12. shared_values$triangulation")
-      print(shared_values$triangulation)
+      #req(input$triangulation)
+      #message("S13. observe parameters")
+      shared_parameters$triangulation <- ifelse(input$triangulation, 1, 0)
+      #message("S13. shared_parameters$triangulation")
+      #print(shared_parameters$triangulation)
     })
     
     # outputs from previous screens----
     
     output$num_innovations_display <- renderText({
-      paste("S13. Number of innovations:", shared_values$num_innovations)
+      paste("Step 3. Number of innovations =", shared_parameters$num_innovations)
     })
     
     output$innovation_system_display <- renderText({
-      if (shared_values$num_innovations == "two_inn") {
-        paste("S13. Innovation System:", shared_values$innovation_system)
-      }
-    })    
+      paste("Step 3. Innovation System =", shared_parameters$innovation_system)
+    })     
     
     output$spatres_display <- renderText({
-      paste("S13. Your spatial resolution is:", shared_values$resolution)
+      paste("Step 2. Spatial resolution =", shared_parameters$resolution)
     })
     
-    
     output$aggregation_display <- renderText({
-      paste("S13. Your aggregation level is:", shared_values$aggregation)
+      paste("Step 2. Aggregation level =", shared_parameters$aggregation)
     })
     
     output$level_display <- renderText({
-      req(shared_values$level)
-      paste("S13. You selected level on Screen 1:", shared_values$level)
+      req(shared_parameters$level)
+      paste("Step 1. Spatial level =", shared_parameters$level)
     })
     
     output$selection_display <- renderText({
-      req(shared_values$level)
+      req(shared_parameters$level)
       
-      if (shared_values$level == "woreda") {
+      if (shared_parameters$level == "woreda") {
         paste(
-          "S13. You selected geography:",
-          shared_values$selected_region,
-          shared_values$selected_zone,
-          shared_values$selected_woreda
+          "Step 1. Geography =",
+          shared_parameters$selected_region,
+          "-",
+          shared_parameters$selected_zone,
+          "-",
+          shared_parameters$selected_woreda
         )
       } else {
-        if (shared_values$level == "zone") {
+        if (shared_parameters$level == "zone") {
           paste(
-            "S13. You selected geography:",
-            shared_values$selected_region,
-            shared_values$selected_zone
+            "Step 1. Geography =",
+            shared_parameters$selected_region,
+            "-",
+            shared_parameters$selected_zone
           )
         } else {
-          if (shared_values$level == "region") {
-            paste("S13. You selected geography:",
-                  shared_values$selected_region)
+          if (shared_parameters$level == "region") {
+            paste("Step 1. Geography =",
+                  shared_parameters$selected_region)
           } else {
-            paste("S13. You selected geography: Ethiopia")
+            paste("Step 1. Geography = Ethiopia")
           }
         }
       }
     }) 
     
     output$crop_1_display <- renderText({
-      message(paste("S13. crop details:", shared_values$crop_name_1))
-      req(shared_values$crop_name_1)
-      paste("S13. You selected crop on Screen 4:", shared_values$crop_name_1)
+      req(shared_parameters$crop_name_1)
+      paste("Step 4. Crop =", shared_parameters$crop_name_1)
     })
     
     
     output$ideotype_1_display <- renderText({
-      message(paste("S13. ideotype details:", shared_values$ideotype_1))
-      req(shared_values$ideotype_1)
-      paste("S13. You selected ideotype on Screen 4:", shared_values$ideotype_1)
+      req(shared_parameters$ideotype_1)
+      paste("Step 4. Ideotype =", shared_parameters$ideotype_1)
     })
     
     
     output$scenario_1_display <- renderText({
-      message(paste("S13. scenario details:", shared_values$scenario_1))
-      req(shared_values$scenario_1)
-      paste("S13. You selected scenario on Screen 4:", shared_values$scenario_1)
+      req(shared_parameters$scenario_1)
+      paste("Step 4. Scenario =", shared_parameters$scenario_1)
     })
     
     
     output$inn_type_1_display <- renderText({
-      message(paste("S13. Innovation type:", shared_values$inn_type_1))
       req(shared_values$inn_type_1)
-      paste("S13. You selected Innovation type on Screen 4:", shared_values$inn_type_1)
+      paste("Step 4. Innovation type =", shared_values$inn_type_1)
     })
+    
+    output$tri_1_display <- renderText({
+      req(shared_parameters$triangulation)
+      paste("Step 13. Triangulation =", shared_parameters$triangulation)
+    })
+    
     
     # _ navigation----
     
@@ -219,19 +290,103 @@ bslib_screen13_module_v3_Server <- function(id, shared_values, switch_screen) {
       switch_screen("screen12")
     })
     
+    # observeEvent add_inn ----
+    observeEvent(input$add_inn, {
+      
+      req(input$add_inn)
+      # if the innovation is not already in the database add a row
+      if (shared_values$inn_type_1 != "existing") {
+        
+        cat("S13. ADD INN BUTTON\n\n")
+        
+        df_inn_old <- read.csv("E:/repos/raise_fs/shiny/data/innovations.csv")
+        
+        cat("\n\ndf_inn_old 1\n\n")
+        print(df_inn_old)  
+        
+        new_inn_id <- (max(df_inn_old$inn_ID) + 1)
+        
+        cat("\n\nnew_inn_id 1\n\n")
+        print(new_inn_id)          
+
+        new_crop <- shared_parameters$crop_name_1
+        
+        cat("\n\nnew_crop\n\n")
+        print(new_crop)
+        
+        new_ideotype <- shared_parameters$ideotype_1
+        
+        cat("\n\nnew_ideotype\n\n")
+        print(new_ideotype)  
+        
+        new_scenario <- shared_parameters$scenario_1
+        
+        cat("\n\nnew_scenario\n\n")
+        print(new_scenario)  
+        
+        df_inn_old <- df_inn_old |> add_row(
+          inn_ID = new_inn_id,
+          crop_name = new_crop,
+          ideotype = new_ideotype,
+          scenario = new_scenario
+        )
+        
+        cat("\n\ndf_inn_old 2\n\n")
+        print(df_inn_old)
+        
+        df_inn_old |> write.csv("E:/repos/raise_fs/shiny/data/innovations.csv", row.names = FALSE)
+
+      }
+    })
+    
     # dynamic run inn2 controls ----
     output$dynamic_run_inn2 <- renderUI({
+      save_progress(shared_values, shared_parameters)
+      showNotification("Progress saved!", type = "message")
       
-      print(paste("num_innovations is", shared_values$num_innovations))
+      print(paste("num_innovations is", shared_parameters$num_innovations))
       
-      if (shared_values$num_innovations == "one_inn") {
+      if (shared_parameters$num_innovations == "one_inn") {
         #tagList(actionButton(ns("run_IRM1_btn"), "Run IRM"))
-        tagList(downloadButton(ns("run_IRM1_btn"), "Run IRM"))
+        tagList(downloadButton(ns("run_IRM1_btn"), "Run IRM",
+                               class = "btn-primary"))
       } else {
-        tagList(actionButton(ns("inn2_btn"), "Choose and Review Innovation 2"))
+        tagList(
+          span(
+            "Choose and Review Innovation 2",
+            style = "color:red; font-size: 12;"
+          )
+        )
       }
       
     })
+    
+    
+    # dynamic dynamic_navbutn controls ----
+    output$dynamic_navbutn <- renderUI({
+      save_progress(shared_values, shared_parameters)
+      showNotification("Progress saved!", type = "message")
+      
+      print(paste("num_innovations is", shared_parameters$num_innovations))
+      
+      if (shared_parameters$num_innovations == "one_inn") {
+        
+      } else {
+        actionButton(
+          ns("to_screen14"),
+          title = "Go to Step 14: Select Innovation 2",
+          label = tagList(
+            #"Go to Screen 2",
+            "Next",
+            # text first
+            icon("circle-right")  # icon second)
+          ),
+          class = "btn-primary"
+        )
+      }
+      
+    })
+    
     
     # # downloadHandler run_IRM1_btn 
     # output$run_IRM1_btn  <- downloadHandler(
@@ -295,12 +450,17 @@ bslib_screen13_module_v3_Server <- function(id, shared_values, switch_screen) {
       # filename = function() {
       #   paste0("report_", Sys.Date(), ".html")
       # },
+      
       filename = paste0(
-            shared_values$crop_name_1,
+            shared_parameters$crop_name_1,
             "_",
-            shared_values$ideotype_1,
+            shared_parameters$ideotype_1,
             "_",
-            shared_values$scenario_1,
+            shared_parameters$scenario_1,
+            "_",
+            shared_parameters$resolution,
+            "_",
+            shared_parameters$DIVCODEVAL,
             ".html"
           ),
       content = function(file) {
@@ -308,26 +468,26 @@ bslib_screen13_module_v3_Server <- function(id, shared_values, switch_screen) {
           params_irm <- list(
             INT = "1",
             SYS = NA,
-            Agg = shared_values$aggregation,
-            MASK = shared_values$resolution,
+            Agg = shared_parameters$aggregation,
+            MASK = shared_parameters$resolution,
             EXT = "Ethiopia",
-            DIVCODEVAR =  shared_values$DIVCODEVAR,
-            DIVCODEVAL =  shared_values$DIVCODEVAL,
-            DIVNAMEVAR =  shared_values$DIVNAMEVAR,
-            SUBDIVNAMEVAR =  shared_values$SUBDIVNAMEVAR,
+            DIVCODEVAR =  shared_parameters$DIVCODEVAR,
+            DIVCODEVAL =  shared_parameters$DIVCODEVAL,
+            DIVNAMEVAR =  shared_parameters$DIVNAMEVAR,
+            SUBDIVNAMEVAR =  shared_parameters$SUBDIVNAMEVAR,
             INN1 =  paste0(
-              shared_values$crop_name_1,
+              shared_parameters$crop_name_1,
               "_",
-              shared_values$ideotype_1,
+              shared_parameters$ideotype_1,
               "_",
-              shared_values$scenario_1
+              shared_parameters$scenario_1
             ),
             RES1 =  3,
-            SOS1 =  shared_values$sos1,
+            SOS1 =  shared_parameters$sos1,
             FAOCLASS1 =  3,
             LIMITS1 =  3,
             CONCCLASS1 =  3,
-            TRIANGULATION = shared_values$triangulation,
+            TRIANGULATION = shared_parameters$triangulation,
             TRILIMITS1 =  "LIMITS",
             TRIDEC1 =  "DECISION",
             TRISUIT1 =  "SUITABILITY",
@@ -341,6 +501,7 @@ bslib_screen13_module_v3_Server <- function(id, shared_values, switch_screen) {
           #   params = params_irm,
           #   envir = results_env
           # )
+
           
           # shared environment
           # Create a clean environment shared between helpers.R and report.Rmd
@@ -358,12 +519,30 @@ bslib_screen13_module_v3_Server <- function(id, shared_values, switch_screen) {
         })
       }
       )
+    # observe triangulation ----
+    # observe({
+    #   req(input$triangulation)
+    #   message(paste("S3. 1 observe: radio buttons", shared_parameters$innovation_system))
+    #   shared_parameters$num_innovations <- input$num_innovations
+    #   
+    # })
     
     # observeEvent inn2_btn ----
     observeEvent(input$inn2_btn, {
-      switch_screen("screen14")
+      shared_values$step <- 14
+      save_progress(shared_values, shared_parameters)
+     switch_screen("screen14")
     })
-
+    
+    # help button 13_01----
+    observeEvent(input$show_help_13_01, {
+      showModal(modalDialog(
+        title = "Step 13: Run IRM or start Innovation 2",
+        includeMarkdown("docs/step_13_01.md"),
+        easyClose = TRUE,
+        footer = modalButton("Close")
+      ))
+    })
       
     })
 }
